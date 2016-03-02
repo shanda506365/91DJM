f045d398deaeaa92fd7c6d7e5469020cfac47654
chenheling <6710582@qq.com>
2016-03-02 10:44:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+DJM 搭积木 Version 1.0
+==== 
+###Example:  <br>
+```javascript
+
---
+++ dist/js/seaConfig.js
@@ -0,0 +1,1 @@
+"use strict";var version=new Date,versionStr=version.getFullYear()+""+(version.getMonth()+1)+version.getDate();seajs.config({base:"/",paths:{image:"../image",css:"../css",js:"../js"},alias:{jquery:"./lib/jquery",mui:"./lib/mui",react:"./lib/react","react-dom":"./lib/react-dom"},map:[[/^(.*\.(?:css|js))(.*)$/i,"$1?v="+versionStr]],charset:"utf-8"});
