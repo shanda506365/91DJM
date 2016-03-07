bfe142b1c0ae3a96694c5480bc168af8a915f06c
chenheling <6710582@qq.com>
2016-03-07 10:10:51 +0800

    3.7

---
+++ dist/js/ThumbView.js
@@ -0,0 +1,1 @@
+"use strict";define(function(require,exports,module){var ThumbView=React.createClass({displayName:"ThumbView",render:function(){return React.createElement("div",null)}});module.exports=ThumbView});
