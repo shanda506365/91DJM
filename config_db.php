1efe7fdf8d45f55bceaf6444edcadb51078253eb
zhouhui <95459910@qq.com>
2016-03-31 10:19:22 +0800

    zhouhui

--- config_db.php
+++ config_db.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'abc123');
+define('DB_HOSTNAME', '42.121.105.2');
+define('DB_USERNAME', 'djm_user');
+define('DB_PASSWORD', 'djm_user!@#');
 define('DB_DATABASE', 'djm');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'djm_');
