41b47c0619b86b7400580885c22f28785c276d93
zhouhui <95459910@qq.com>
2016-03-31 10:43:19 +0800

    zhouhui

--- admin/language/zh-CN/design/banner.php
+++ admin/language/zh-CN/design/banner.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']      = '横幅管理';
+$_['heading_title']      = '广告位管理';
 
 // Text
-$_['text_success']       = '成功：您已修改横幅模块！';
-$_['text_list']          = '横幅列表';
-$_['text_add']           = '添加横幅';
-$_['text_edit']          = '编辑横幅';
+$_['text_success']       = '成功：您已修改广告位！';
+$_['text_list']          = '广告位列表';
+$_['text_add']           = '添加广告位';
+$_['text_edit']          = '编辑广告位';
 $_['text_default']       = '默认';
 
 
 // Column
-$_['column_name']        = '横幅名称';
+$_['column_name']        = '广告位名称';
 $_['column_status']      = '状态';
 $_['column_action']      = '管理';
 
 // Entry
-$_['entry_name']         = '横幅名称：';
+$_['entry_name']         = '广告位名称：';
 $_['entry_title']        = '标题：';
 $_['entry_link']         = '链接：';
 $_['entry_image']        = '图片：';
@@ -24,6 +24,6 @@ $_['entry_status']       = '状态：';
 $_['entry_sort_order']   = '排序';
 
 // Error
-$_['error_permission']   = '警告： 您没有权限修改横幅！';
-$_['error_name']         = '横幅必须在 3 至 64个字符之间！';
-$_['error_title']        = '横幅标题必须在 2 至 64个字符之间！';
+$_['error_permission']   = '警告： 您没有权限修改广告位！';
+$_['error_name']         = '广告位必须在 3 至 64个字符之间！';
+$_['error_title']        = '标题必须在 2 至 64个字符之间！';
