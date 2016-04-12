7ed197094de10a3e79353079e75edc97b626fd06
zhouhui <95459910@qq.com>
2016-04-12 14:40:27 +0800

    收藏

---
+++ catalog/controller/api/collect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 周
+ * Date: 2016/4/12
+ * Time: 14:39
+ */
+class ControllerApiCollect extends Controller
+{
+    public function index()
+    {
+        $this->load->language('api/login');
+    }
+}
